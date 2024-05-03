a943b6572cdafbf7639d50105b122865b6f58ec2
andrew-marquezin <andrewmmarinho@gmail.com>
2024-05-03 13:58:52 -0300

    revisão do requisito 1 e finalizado requisito 2

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,10 +2,11 @@ from src.pre_built.counter import count_ocurrences
 
 
 path = "data/jobs.csv"
-word = "Python"
 
 
 def test_counter():
     '''testa a execução da função "count_ocurrences"'''
-    counter = count_ocurrences(path, word)
-    assert counter == 1639
+    python_counter = count_ocurrences(path, "python")
+    js_counter = count_ocurrences(path, "javascript")
+    assert python_counter == 1639
+    assert js_counter == 122
