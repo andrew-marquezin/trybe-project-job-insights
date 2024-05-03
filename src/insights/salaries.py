7a4af5f849338ddfc59b069cd3cc4d829c53c21b
andrew-marquezin <andrewmmarinho@gmail.com>
2024-05-03 15:22:55 -0300

    requisito 8 feito

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -25,7 +25,15 @@ class ProcessSalaries(ProcessJobs):
         return min(min_salary_list)
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
-        pass
+        if "min_salary" not in job or "max_salary" not in job:
+            raise ValueError("job doesn't have salary info")
+        min_salary, max_salary = job["min_salary"], job["max_salary"]
+
+        if not isinstance(min_salary, int) or not isinstance(max_salary, int):
+            raise ValueError("salary must be a number!")
+        if min_salary > max_salary:
+            raise ValueError("min salary is higher than max salary")
+        return min_salary <= int(salary) <= max_salary
 
     def filter_by_salary_range(
         self, jobs: List[dict], salary: Union[str, int]
